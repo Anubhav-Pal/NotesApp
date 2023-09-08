def7ac5da356f4e346e57466c1fa354229100480
Anubhav Pal <anubhavpal15june@gmail.com>
2023-09-08 19:57:38 +0530

    Progress commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { useEffect, useState } from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  // States
+  const [notes, setNotes] = useState(null);
+  const [createForm, setCreateFrom] = useState({
+    title: '',
+    body: ''
+  })
+
+  // useeffects
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch('http://localhost:3000/notes')
+      const data = await response.json();
+      console.log(data);
+      setNotes(data);
+    };
+    fetchData();
+  }, [])
+
+  // Functions
+  const updateCreateFormField = (e) => {
+    const { name, value } = e.target;
+    // console.log({ name, value });
+
+    setCreateFrom({
+      ...createForm,
+      [name]: value
+    });
+  }
+
+  const createNote = async (e) => {
+    e.preventDefault();
+    await fetch('http://localhost:3000/notes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(createForm)
+    })
+      .then(response => {
+        response.json()
+        setNotes(...notes, response.data.note)
+      })
+      .then(data => {
+        console.log('Response:', data)
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+
+    // const data = await Response.data.json();
+    // // setNotes(...notes, data.note)
+    // console.log(data);
+
+
+    console.log("Form submitted");
+  }
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      {notes && notes.map((note) =>
+        <div key={note._id}>
+          <h1>{note.title}</h1>
+          <h1>{note.body}</h1>
+        </div>
+      )}
+
+      <form onSubmit={createNote}>
+        <input type="text" value={createForm.title} onChange={updateCreateFormField} name='title' />
+        <br />
+        <textarea value={createForm.body} onChange={updateCreateFormField} name="body" id="" cols="30" rows="10"></textarea>
+        <br />
+        <button type="submit">Create</button>
+      </form>
+    </div>
   )
 }
 
