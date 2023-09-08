da5fc80d2e1bcceb384a931df96f8e240bcac8fc
Anubhav Pal <anubhavpal15june@gmail.com>
2023-09-09 01:23:38 +0530

    Delete functionality added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -35,42 +35,67 @@ const App = () => {
     });
   }
 
+
+  //Add a note
   const createNote = async (e) => {
     e.preventDefault();
-    let Data=''
-     await fetch('http://localhost:3000/notes', {
+    let Data = ''
+    await fetch('http://localhost:3000/notes', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(createForm)
+      body: JSON.stringify(createForm)     //The Js object that has recently been in the createForm object will be added to the db
     })
       .then(response => { return response.json() })
-      .then(data => { Data=data.note})
+      .then(data => { Data = data.note })
       .catch(error => { console.error('Error:', error); });
 
+    //Update state
     setNotes([...notes, Data]);
+
+    //Clear the form fields
+    setCreateFrom({ title: '', body: '' })
     console.log("Form submitted");
+  }
+
+  //Delete a note
+  const deleteNote = async (_id) => {
+    try {
+      await fetch(`http://localhost:3000/notes/${_id}`, {
+        method: "DELETE",
+        headers: {
+          'Content-Type': 'application/json',
+        }
+      })
+        .then(response => { return response.json() })
+        .catch((error) => console.log(error))
+
+    } catch (error) { console.log(error); }
+
+    //Update the state
+    setNotes((notes) => notes.filter((note) => note._id !== _id));
   }
 
 
 
   return (
     <div>
+      <form onSubmit={createNote}>
+        <input type="text" value={createForm.title} onChange={updateCreateFormField} name='title' required={true} />
+        <br />
+        <textarea value={createForm.body} onChange={updateCreateFormField} name="body" id="" cols="30" rows="10" required={true}></textarea>
+        <br />
+        <button type="submit">Create</button>
+      </form>
+
       {notes && notes.map((note) =>
         <div key={note._id}>
           <h1>{note.title}</h1>
           <h1>{note.body}</h1>
+          <button onClick={() => deleteNote(note._id)}>Delete</button>
         </div>
       )}
-
-      <form onSubmit={createNote}>
-        <input type="text" value={createForm.title} onChange={updateCreateFormField} name='title' />
-        <br />
-        <textarea value={createForm.body} onChange={updateCreateFormField} name="body" id="" cols="30" rows="10"></textarea>
-        <br />
-        <button type="submit">Create</button>
-      </form>
     </div>
   )
 }
