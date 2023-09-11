08f06d1147447e935f98e951b65ea166b7180cee
Anubhav Pal <anubhavpal15june@gmail.com>
2023-09-12 00:04:46 +0530

    Create Note UI progress commit

--- Frontend/src/components/CreateNote.jsx
+++ Frontend/src/components/CreateNote.jsx
@@ -5,8 +5,8 @@ const CreateNote = (props) => {
     <div className='create-note-form'>
       <form onSubmit={props.onSubmitFunction}>
         <div className=' flex flex-col items-start m-4 gap-2'>
-          <input className='w-full p-4 rounded-2xl bg-transparent border-2 border-zinc-800' placeholder='Enter note title' type="text" value={props.title} onChange={props.onChangeFunction} name='title' required={true} />
-          <textarea className='w-full p-4 rounded-2xl bg-transparent border-2 border-zinc-800' placeholder="Enter note content" value={props.body} onChange={props.onChangeFunction} name="body" id="" cols="30" rows="10" required={true}></textarea>
+          <input className='w-full p-4 rounded-2xl bg-transparent border-2 border-zinc-800 text-white placeholder:text-zinc-600 focus:outline-none focus:border-zinc-600' placeholder='Enter note title' type="text" value={props.title} onChange={props.onChangeFunction} name='title' required={true} />
+          <textarea className='w-full p-4 rounded-2xl bg-transparent border-2 border-zinc-800 text-white placeholder:text-zinc-600 focus:outline-none focus:border-zinc-600' placeholder="Enter note content" value={props.body} onChange={props.onChangeFunction} name="body" id="" cols="30" rows="10" required={true}></textarea>
           <button type="submit">Create</button>
         </div>
       </form>
